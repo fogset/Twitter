1c895049e1ee18c2ebbf0b22543864f35029b461
Tianhao <zhangth1990@hotmail.com>
2018-07-08 22:45:17 -0600

    add Firebase

--- Twitter/app/src/main/java/com/example/tianhao/twitter/loginActivity.java
+++ Twitter/app/src/main/java/com/example/tianhao/twitter/loginActivity.java
@@ -3,7 +3,7 @@ package com.example.tianhao.twitter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
