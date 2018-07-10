5e8ec106c51c9f8a0b2dfdc3efe3c29a8bd2b94e
Tianhao <zhangth1990@hotmail.com>
2018-07-10 00:29:44 -0600

    following one user

--- Twitter/app/src/main/java/com/example/tianhao/twitter/UsersActivity.java
+++ Twitter/app/src/main/java/com/example/tianhao/twitter/UsersActivity.java
@@ -12,6 +12,8 @@ import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,7 +24,12 @@ import java.util.ArrayList;
 
 public class UsersActivity extends AppCompatActivity {
     ArrayList<String> users = new ArrayList<>();
+    ArrayList<String> usersName = new ArrayList<>();
     ArrayAdapter adapter;
+    private FirebaseAuth mAuth;
+    String currentEmail;
+    FirebaseUser currentUser;
+    String[] currentLogINUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +38,10 @@ public class UsersActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.userListView);
         listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        currentEmail = currentUser.getEmail();
+        currentLogINUser = currentEmail.split("\\.");
 
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_checked, users);
         listView.setAdapter(adapter);
@@ -40,6 +51,7 @@ public class UsersActivity extends AppCompatActivity {
                 CheckedTextView checkedTextView = (CheckedTextView) view;
                 if (checkedTextView.isChecked()) {
                     Log.i("Info", "Checked!");
+                    FirebaseDatabase.getInstance().getReference().child("users").child(currentLogINUser[0]).child("isFollowing").setValue(users.get(position));
                 } else {
                     Log.i("Info", "Not Checked!");
                 }
@@ -48,15 +60,16 @@ public class UsersActivity extends AppCompatActivity {
 
 
         FirebaseDatabase.getInstance().getReference().child("users").addChildEventListener(new ChildEventListener() {
-
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                for (DataSnapshot messageFireBase : dataSnapshot.getChildren()) {
-                    String messageReceived = String.valueOf(messageFireBase.getValue());
-                    if (messageReceived != null) {
-                        users.add(messageReceived);
+                for (DataSnapshot emailFireBase : dataSnapshot.getChildren()) {
+                    String emailReceived = String.valueOf(emailFireBase.getValue());
+                    if (emailReceived != null) {
+                        users.add(emailReceived);
+                        String[] tempUserName = emailReceived.split("\\.");
+                        usersName.add(tempUserName[0]);
                     }
-                    Log.i("receive message", messageReceived);
+                    Log.i("receive message", emailReceived);
                 }
                 adapter.notifyDataSetChanged();
             }
