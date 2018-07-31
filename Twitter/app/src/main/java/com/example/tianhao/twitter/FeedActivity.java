80f9d274cf58c59d7464ed54e833b5d17a06a104
Tianhao <zhangth1990@hotmail.com>
2018-07-31 01:42:57 -0600

    fixed error with a simpler solution without sharedPereference

--- Twitter/app/src/main/java/com/example/tianhao/twitter/FeedActivity.java
+++ Twitter/app/src/main/java/com/example/tianhao/twitter/FeedActivity.java
@@ -26,60 +26,57 @@ import java.util.Map;
 
 public class FeedActivity extends AppCompatActivity {
     User user;
-    UsersTweets mApp;
-     ArrayList<String> usersTweets = new ArrayList<>();
-     ArrayList<String> usersEmail = new ArrayList<>();
-    ArrayAdapter adapter;
+    ListView listView;
+    //String[] usersTweets,usersEmail;
+    ArrayList<String> usersTweets = new ArrayList<>();
+    ArrayList<String> usersEmail = new ArrayList<>();
     SharedPreferences sharedPreferences;
     String entireTweets ;
     String entireEmailTweets;
+
     @SuppressLint("LongLogTag")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
 
-
         user = new User();
-        mApp = (( UsersTweets)getApplicationContext());
-
-
         sharedPreferences = this.getSharedPreferences("com.example.tianhao.twitter", Context.MODE_PRIVATE);
 
+        ListView listView = findViewById(R.id.listView);
+        final List<Map<String, String>> tweetData = new ArrayList<>();
 
+        final SimpleAdapter simpleAdapter = new SimpleAdapter(this, tweetData, android.R.layout.simple_list_item_2, new String[]{"content", "username"}, new int[]{android.R.id.text1, android.R.id.text2});
+        listView.setAdapter(simpleAdapter);
 
 
         FirebaseDatabase.getInstance().getReference().child("users").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
                 String entireTweets = "";
                 String entireEmailTweets = "";
                 for (DataSnapshot emailFireBase : dataSnapshot.child("emailList").getChildren()) {
                     user = emailFireBase.getValue(User.class);
                     entireTweets = entireTweets + "#" + user.getTweets();
                     entireEmailTweets = entireEmailTweets + "#" + user.getEmail();
+                    usersTweets.add(user.getTweets());
+                    usersEmail.add(user.getEmail());
                 }
-
+                for (int a = 0; a<4; a++) {
+                    Map<String, String> tweetInfo = new HashMap<>();
+                    tweetInfo.put("content",  usersTweets.get(a));
+                    tweetInfo.put("username",  usersEmail.get(a));
+                    tweetData.add(tweetInfo);
+                }
+                simpleAdapter.notifyDataSetChanged();
                 sharedPreferences.edit().putString("userTweets", entireTweets).apply();
                 sharedPreferences.edit().putString("userTweetsEmail", entireEmailTweets).apply();
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) { }
         });
-//        for (int i = 0; i<3; i++){
-//            //tweetInfo.put("content", usersTweets.get(i));
-//            //tweetInfo.put("username", usersEmail.get(i));
-//            Log.i("tweets", usersTweets.get(i));
-//            Log.i("email", usersEmail.get(i));
-//            //tweetData.add(tweetInfo);
-//            //simpleAdapter.notifyDataSetChanged();
-//        }
-        //String wholeString = sharedPreferences.getString("wholeString","");
-        //simpleAdapter.notifyDataSetChanged();
-        //Log.i("emailaooo", wholeString);
 
-        //Log.i("second tweets outside class", String.valueOf(((UsersTweets)getApplicationContext()).getUsersTweets()));
+
 
     }
 
@@ -87,29 +84,44 @@ public class FeedActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
-        new CountDownTimer(2000,1000){
+
+        new CountDownTimer(1000,1000){
           public  void onTick(long millisecondsUntilDone){
           }
           public void onFinish(){
-              Log.i("We are done!", "No more countdown");
               entireTweets = sharedPreferences.getString("userTweets","");
               entireEmailTweets = sharedPreferences.getString("userTweetsEmail","");
               Log.i("emailLList", entireEmailTweets);
               Log.i("emailLTweet", entireTweets);
+              usersTweets= entireTweets.split("\\#");
+              usersEmail= entireEmailTweets.split("\\#");
+              for (int i = 1; i <usersTweets.length; i++) {
+                  Map<String, String> tweetInfo = new HashMap<>();
+                  tweetInfo.put("content",  usersTweets[i] );
+                  tweetInfo.put("username",  usersEmail[i]);
+                  tweetData.add(tweetInfo);
+              }
+              simpleAdapter.notifyDataSetChanged();
           }
         }.start();
-         ListView listView = findViewById(R.id.listView);
-         List<Map<String, String>> tweetData = new ArrayList<>();
-         Map<String, String> tweetInfo = new HashMap<>();
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, tweetData, android.R.layout.simple_list_item_2, new String[]{"content", "username"}, new int[]{android.R.id.text1, android.R.id.text2});
-        listView.setAdapter(simpleAdapter);
-        for (int i = 1; i <= 5; i++) {
-            tweetInfo.put("content", "Tweet Content" + Integer.toString(i));
-            tweetInfo.put("username", "User" + Integer.toString(i));
+        for (int i = 0; i <= 3; i++) {
+            tweetInfo.put("content", "Tweet Content" +String.valueOf(i) );
+            tweetInfo.put("username", "User" + String.valueOf(i));
             tweetData.add(tweetInfo);
         }
-        simpleAdapter.notifyDataSetChanged();
+        simpleAdapter = new SimpleAdapter(FeedActivity.this, tweetData, android.R.layout.simple_list_item_2, new String[]{"content", "username"}, new int[]{android.R.id.text1, android.R.id.text2});
+        listView.setAdapter(simpleAdapter);
+
+        for (int i = 0; i <= 2; i++) {
+            tweetInfo.put("content", "Tweet Content" +usersTweets[i] );
+            tweetInfo.put("username", "User" + usersEmail[i]);
+            tweetData.add(tweetInfo);
+        }
+
+
+
+
+
 
 
 
